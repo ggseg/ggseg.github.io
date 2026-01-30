ada7119656ef2e50845dc55c7dd7ca1a483b0874
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2026-01-31 00:29:43 +0100

    add vignette links

--- R/fetch-data.R
+++ R/fetch-data.R
@@ -25,7 +25,7 @@ get_packages <- function(use_cache = TRUE) {
       stars = `_stars`,
       dplyr::any_of(c("_contributors", "_readme", "_pkglogo"))
     ) |>
-    dplyr::rename_with(~ "logo", dplyr::any_of("_pkglogo")) |>
+    dplyr::rename_with(~"logo", dplyr::any_of("_pkglogo")) |>
     dplyr::mutate(
       github_url = dplyr::case_when(
         grepl("github.com", url) ~ sub(",.*", "", url),
@@ -40,22 +40,60 @@ get_packages <- function(use_cache = TRUE) {
   packages
 }
 
-get_core_packages <- function() {
-  packages <- get_packages()
-  core_names <- c("ggseg", "ggseg3d", "ggsegExtra")
-  packages |>
+get_core_packages <- function(
+  core_names = c("ggseg", "ggseg3d", "ggsegExtra"),
+  ...
+) {
+  get_packages(...) |>
     dplyr::filter(package %in% core_names) |>
     dplyr::arrange(match(package, core_names))
 }
 
-get_atlas_packages <- function() {
-  packages <- get_packages()
+get_atlas_packages <- function(...) {
+  packages <- get_packages(...)
+  core <- get_core_packages(...)$package
   packages |>
     dplyr::filter(
       grepl("^ggseg", package),
-      !package %in% c("ggseg", "ggseg3d", "ggsegExtra")
+      !package %in% core
     ) |>
     dplyr::arrange(package)
+}
+
+get_vignettes <- function() {
+  url <- "https://ggseg.r-universe.dev/api/packages"
+  response <- jsonlite::fromJSON(url, flatten = TRUE)
+  response <- response[grep("^ggseg", response$Package), ]
+
+  vignettes_list <- lapply(seq_len(nrow(response)), function(i) {
+    pkg <- response$Package[i]
+    vigs <- response$`_vignettes`[[i]]
+    if (is.null(vigs) || length(vigs) == 0) {
+      return(NULL)
+    }
+    vigs$package <- pkg
+    vigs$url <- paste0(
+      "https://ggseg.github.io/",
+      pkg,
+      "/articles/",
+      sub("\\.Rmd$", ".html", vigs$source)
+    )
+    vigs
+  })
+
+  vignettes <- dplyr::bind_rows(vignettes_list)
+  if (nrow(vignettes) == 0) {
+    return(data.frame(
+      package = character(),
+      title = character(),
+      source = character(),
+      url = character()
+    ))
+  }
+
+  vignettes |>
+    dplyr::select(package, title, source, url) |>
+    dplyr::arrange(package, title)
 }
 
 get_contributors <- function() {
